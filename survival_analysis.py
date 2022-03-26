0469517352af0d409a30bd98fc93e185f39ceeac
chunxu <41453752+chunxu@users.noreply.github.com>
2022-03-25 20:35:09 -0500

    prepare data

--- survival_analysis.py
+++ survival_analysis.py
@@ -1,1 +1,48 @@
 #Survival analysis and estimation of hazard ratios in different variants
+import pandas as pd
+import numpy as np
+import seaborn as sns
+import matplotlib.pyplot as plt
+import warnings
+%matplotlib inline
+warnings.filterwarnings('ignore')
+
+#Loading the dataset
+df = pd.read_csv('survival_data.csv')
+#see what the data look like
+df.head() # all three var and time duration are continuous numbers
+
+df.shape
+# statistical information on the numbers in the dataset
+df.describe()
+# datatype information
+df.info()
+# check for null values
+df.isnull().sum() #perfect! No null values found.
+
+# create box plots to check outliers:
+fig, ax = plt.subplots(ncols=5, nrows=1, figsize=(10, 5))
+index = 0
+ax = ax.flatten()
+
+for col, value in df.items():
+    sns.boxplot(y=col, data=df, ax=ax[index])
+    index += 1
+plt.tight_layout(pad=0.5, w_pad=0.5, h_pad=5.0) 
+
+# create dist plot to see distribution: 
+fig, ax = plt.subplots(ncols=5, nrows=1, figsize=(10, 5))
+index = 0
+ax = ax.flatten()
+
+for col, value in df.items():
+    sns.distplot(value, ax=ax[index])
+    index += 1
+plt.tight_layout(pad=0.5, w_pad=0.5, h_pad=5.0)
+
+
+#Corelation 
+corr = df.corr()
+plt.figure(figsize=(20,10))
+sns.heatmap(corr, annot=True, cmap='coolwarm')
+# all three variables are independent
