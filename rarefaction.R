3d358edade370e91aaf06d672ff66598fb35bdf2
chunxu <41453752+chunxu@users.noreply.github.com>
2022-04-29 21:18:37 -0500

    Create rarefaction.R

---
+++ rarefaction.R
@@ -0,0 +1,14 @@
+rbiom was developed by Dan at CMMR
+#install.packages("rbiom")
+library(rbiom)
+
+#load file
+biom <- rbiom::read.biom("C:\gao\BCM2021\MILES\Pool912\p912-Goodarzi.metaphlan2.Bacteria.EstCount.txt") 
+
+#normalize data by rarefaction
+biom <- rbiom::rarefy(biom)
+
+
+#rbiom::depth(biom)
+rbiom::write.biom(biom, "p912-Goodarzi.metaphlan2.Bacteria.EstCount.rare.txt", format = "tab")
+#getwd() #where the file is.
